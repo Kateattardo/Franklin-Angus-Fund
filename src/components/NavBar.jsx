c6210f6ce7294ddc4cf2fd929275c5ba744ee10b
Kateattardo <youremail@domain.com>
2024-01-04 11:13:26 -0500

    navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,10 @@
-import React, { useState, useEffect } from "react";
-import { FaBars } from "react-icons/fa";
-import { FaFacebookF, FaInstagram } from "react-icons/fa";
+import React, { useState, UseEffect } from "react";
+import { FaFacebookF, FaInstagram, FaBars } from "react-icons/fa";
 
 const NavBar = () => {
   return (
-    <div className="w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white bg-grey-700/80">
-      <ul className="hidden sm:flex px-4">
+    <div className="w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white bg-gray-700/80">
+      <ul>
         <li>
           <a href="/">Home</a>
         </li>
@@ -19,18 +18,18 @@ const NavBar = () => {
           <a href="#benificiaries">Benificiaries</a>
         </li>
         <li>
-          <a href="#donations">Donations</a>
+          <a href="#contact">Contact</a>
         </li>
       </ul>
-      <div className="flex justify-between">
+      <div>
         <FaFacebookF className="mx-4" />
         <FaInstagram className="mx-4" />
       </div>
-      {/*Hamburger Icon */}
-      <div className="sm:hidden z-10">
-        <FaBars size={20} className="mr-4 cursor-pointer" />
+      {/*Hamburger Icon*/}
+      <div>
+        <FaBars />
       </div>
-      {/*Mobile Menu */}
+      {/*Mobil Menu*/}
       <div>
         <ul>
           <li>
@@ -46,7 +45,7 @@ const NavBar = () => {
             <a href="#benificiaries">Benificiaries</a>
           </li>
           <li>
-            <a href="#donations">Donations</a>
+            <a href="#contact">Contact</a>
           </li>
         </ul>
       </div>
