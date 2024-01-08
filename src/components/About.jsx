77fe2447876f680bb519836fafb62934e594b741
Kateattardo <youremail@domain.com>
2024-01-08 14:40:45 -0500

    About page starter

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,11 +2,16 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
+    <>
       <div className="w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white bg-blue-700/80">
-        <h1>About</h1>
+        <h1 className="text-3xl font-bold">About</h1>
       </div>
-    </div>
+      <div className="p-4 mt-12">
+        <p className="text-base text-gray-700">
+          Here is the write up for Franklin Angus Fund and all it's details
+        </p>
+      </div>
+    </>
   );
 };
 
