55edb4c21c57c0c46f5637df6f9f65d4d5a0d25c
Kateattardo <youremail@domain.com>
2024-01-08 10:34:44 -0500

    navbar/hamburger/mobil menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,9 +2,13 @@ import React, { useState, UseEffect } from "react";
 import { FaFacebookF, FaInstagram, FaBars } from "react-icons/fa";
 
 const NavBar = () => {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
   return (
     <div className="w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white bg-gray-700/80">
-      <ul>
+      <ul className="hidden sm:flex px-4">
         <li>
           <a href="/">Home</a>
         </li>
@@ -21,30 +25,37 @@ const NavBar = () => {
           <a href="#contact">Contact</a>
         </li>
       </ul>
-      <div>
+      <div className="flex justify-between">
         <FaFacebookF className="mx-4" />
         <FaInstagram className="mx-4" />
       </div>
       {/*Hamburger Icon*/}
-      <div>
-        <FaBars />
+      <div onClick={handleNav} className="sm:hidden z-10">
+        <FaBars size={20} className="mr-4 cursor-pointer" />
       </div>
       {/*Mobil Menu*/}
-      <div>
-        <ul>
-          <li>
+      <div
+        onClick={handleNav}
+        className={
+          nav
+            ? "overflow-y-hidden md:hidden ease-in duration-300 absolute text-gray-300 left-0 top-0 w-full h-screen bg-black/90 px-4 py-7 flex flex-col"
+            : "absolute top -0 h-screen left-[-100%] eses-in duration-500 "
+        }
+      >
+        <ul className="h-full w-full text-center pt-12">
+          <li className="text-2xl py-8">
             <a href="/">Home</a>
           </li>
-          <li>
+          <li className="text-2xl py-8">
             <a href="#about">About</a>
           </li>
-          <li>
+          <li className="text-2xl py-8">
             <a href="#application">Application</a>
           </li>
-          <li>
+          <li className="text-2xl py-8">
             <a href="#benificiaries">Benificiaries</a>
           </li>
-          <li>
+          <li className="text-2xl py-8">
             <a href="#contact">Contact</a>
           </li>
         </ul>
